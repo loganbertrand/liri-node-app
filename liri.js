7756b013d1b29c9a2b60dcffcbc26d3f763c8e30
Logan Bertrand <loganwbertrand@gmail.com>
2019-04-29 15:08:56 -0700

    added the initial files, and npm install

---
+++ liri.js
@@ -0,0 +1,21 @@
+//LIRI JS
+
+//dotenv required
+require("dotenv").config();
+
+//key.js file
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+//Process commands
+
+
+//concert-this
+
+//spotify-this-song
+
+//movie-this
+
+//do-what-it-says
+
